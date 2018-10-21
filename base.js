e342d982f2cabe3f59ad6167a489f717f3e8ccb5
moquan0116 <330898472@qq.com>
2018-10-21 21:04:14 +0800

    新建index.html,base.js

---
+++ base.js
@@ -0,0 +1,11 @@
+function inherit(p) {
+    if(p == null) throw TypeError();
+    if(Object.create)
+        return Object.create(p);
+    var t = typeof p;
+    if(t != 'Object' && t !== 'function') throw TypeError();
+    function f() {};
+    f.prototype = p;
+    return new f();
+
+}
