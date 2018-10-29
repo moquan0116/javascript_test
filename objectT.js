564667edd5ef8630f3401813fb29ae1f0439b2e0
moquan0116 <330898472@qq.com>
2018-10-29 22:33:52 +0800

    一个Card类,不明白嘛意思

--- objectT.js
+++ objectT.js
@@ -28,9 +28,10 @@ console.log(c);*/
 
 var Coin = enumeration({Penny: 1, Nicke: 5, Dime: 10, Quarter: 25});
 var c = Coin.Dime;
+// console.log(c instanceof Coin);
+// console.log(c.constructor == Coin);
 
 function enumeration(namesToValues) {
-    console.log(namesToValues);
     var enumeration = function () {
         throw "Can't Instantiate Enumerations";
     };
@@ -64,29 +65,92 @@ function enumeration(namesToValues) {
 
     return enumeration;
 }
-function Base() {
-    this.c = 100;
-}
-var f = function () {
-    this.a = 1;
-    this.add = function (num) {
-        this.a+=num;
-    }
-};
-f.prototype.base = new Base();
-function N() {
-    this.b = 1;
-}
-N.prototype.add = function (num) {
-    this.b+=num;
-};
-N.prototype.edit = function (num) {
-    this.c=num;
-};
-N.prototype.base = new Base();
-var a1 = new f();
-var b2 = new N();
-b2.edit(888);
-console.log(a1);
-console.log(b2);
 
+function Card(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+}
+
+Card.Suit = enumeration({
+    Clubs: 1,
+    Diamonds: 2,
+    Hearts: 3,
+    Spades: 4
+});
+
+Card.Rank = enumeration({
+    Two: 2,
+    Three: 3,
+    Four: 4,
+    Five: 5,
+    Six: 6,
+    Seven: 7,
+    Eight: 8,
+    Nine: 9,
+    Ten: 10,
+    Jack: 11,
+    Queen: 12,
+    King: 13,
+    Ace: 14
+});
+
+Card.prototype.toString = function () {
+    return this.rank.toString() + " of " + this.suit.toString();
+};
+
+Card.prototype.compareTo = function (that) {
+    if (this.rank < that.rank) return -1;
+    if (this.rank > that.rank) return 1;
+    return 0;
+};
+
+Card.orderByRank = function (a, b) {
+    return a.compareTo(b);
+};
+
+Card.orderBySuit = function (a, b) {
+    if (a.suit < b.suit) return -1;
+    if (a.suit > b.suit) return 1;
+    if (a.rank < b.rank) return -1;
+    if (a.rank > b.rank) return 1;
+    return 0;
+};
+
+function Deck() {
+    var cards = this.cards = [];
+    Card.Suit.foreach(function (s) {
+        Card.Rank.foreach(function (r) {
+            cards.push(new Card(s, r));
+        })
+    });
+};
+
+Deck.prototype.shuffle = function () {
+    var deck = this.cards, len = deck.length;
+    for (var i = len - 1; i > 0; i--) {
+        var r = Math.floor(Math.random() * (i + 1)), temp;
+        temp = deck[i], deck[i] = deck[r], deck[r] = temp;
+    }
+    return this;
+};
+
+Deck.prototype.deal = function (n) {
+    if (this.cards.length < n) throw "Out of cards";
+    return this.cards.splice(this.cards.length - n, n);
+};
+
+var deck = (new Deck()).shuffle();
+var hand = deck.deal(13).sort(Card.orderBySuit);
+
+
+f/*unction Long (num) {
+    this.num = num;
+};
+
+Long.prototype.add = function (n) {
+    this.num+=n;
+};
+
+var a = new Long(1);
+var res = 'num' in a;
+console.log(res);*/
